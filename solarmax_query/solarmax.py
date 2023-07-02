f7bf7ab912b5875e1d7afe2420507736e6e11fbd
Dominik Wetzel <dominik@wetzel.one>
2023-07-02 19:51:41 +0200

    Use snake case for variable and function names

--- solarmax_query/solarmax.py
+++ solarmax_query/solarmax.py
@@ -2,11 +2,11 @@ import socket, subprocess, os, time
 
 
 class SolarMax:
-    def __init__(self, host: str, port: int = 12345, inverterIndex: int = 1) -> None:
+    def __init__(self, host: str, port: int = 12345, inverter_index: int = 1) -> None:
         self.socket = socket.socket(
             socket.AF_INET, socket.SOCK_STREAM
         )  # type: socket.socket
-        self.index = inverterIndex
+        self.index = inverter_index
         self.host = host
         self.port = port
         self.connect()
@@ -15,7 +15,7 @@ class SolarMax:
         if self.socket is not None:
             self.socket.close()
 
-    def pingInverter(self) -> bool:
+    def ping_inverter(self) -> bool:
         if os.name == "nt":
             out = subprocess.Popen(
                 ["ping", "-n", "1", self.host],
@@ -33,12 +33,12 @@ class SolarMax:
 
     def reconnect(self) -> None:
         self.__del__()
-        while not self.pingInverter():
+        while not self.ping_inverter():
             time.sleep(60)
         self.connect()
 
     def connect(self) -> None:
-        if not self.pingInverter():
+        if not self.ping_inverter():
             raise Exception("Inverter not reachable")
         try:
             self.socket.connect((self.host, self.port))
@@ -46,7 +46,6 @@ class SolarMax:
             self.socket.close()
             self.socket = None
             raise Exception(f"Could not connect to host: {self.host}:{self.port}")
-
 
     def checksum(self, text: str) -> str:
         total = 0
@@ -57,29 +56,29 @@ class SolarMax:
             crc = "0" + crc
         return crc
 
-    def createQueryString(self, code: str) -> str:
+    def create_query_string(self, code: str) -> str:
         # For the structure see 1.1
 
         # FB is hex for 251 wich is the reserved aress for an outsite host wich we are see 1.3
-        srcAddress = "FB"
+        src_address = "FB"
 
         # inverterIndex is the index of the inverter we want to query wich is converted to hex wich has to be 2 characters long see 1.1
-        destAddress = f"{self.index:02X}"
+        dest_address = f"{self.index:02X}"
 
         # this the "port" in hex wich for data query is always 100 see 1.4
-        queryType = "64"
+        query_type = "64"
 
         length = 1 + 3 + 3 + 3 + 3 + len(code) + 1 + 4 + 1
         length = f"{length:02X}"
-        preCrcString = f"{srcAddress};{destAddress};{length}|{queryType}:{code}|"
+        pre_crc_string = f"{src_address};{dest_address};{length}|{query_type}:{code}|"
 
         # this is the checksum of the query see 1.1
-        crc = self.checksum(preCrcString)
-        queryString = "{" + preCrcString + crc + "}"
+        crc = self.checksum(pre_crc_string)
+        query_string = "{" + pre_crc_string + crc + "}"
 
-        return queryString
+        return query_string
 
-    def parseData(self, data: str) -> int:
+    def parse_data(self, data: str) -> int:
         # data is the data from the inverter
         # for example "{01;FB;18|64:ADR=1|04A9}"
         # we are only interested in the data part
@@ -94,11 +93,11 @@ class SolarMax:
         return int(ndata, 16)
 
     def query(self, code: str) -> int:
-        queryString = self.createQueryString(code)
+        query_string = self.create_query_string(code)
 
         try:
             # send query
-            self.socket.sendall(queryString.encode())
+            self.socket.sendall(query_string.encode())
             # recive query
             data = ""
             while len(data) < 1:
@@ -107,39 +106,39 @@ class SolarMax:
             return None
 
         # check crc
-        inCrc = data[-5:-1]
-        checkCrc = self.checksum(data[1:-5])
-        if inCrc != checkCrc:
+        in_crc = data[-5:-1]
+        check_crc = self.checksum(data[1:-5])
+        if in_crc != check_crc:
             return None
 
         # parse data
-        return self.parseData(data)
+        return self.parse_data(data)
 
-    def getUnit(self, funktion: object) -> str:
+    def get_unit(self, function: object) -> str:
         units = {
-            self.acOutput: "W",
-            self.operatingHours: "h",
-            self.dateYear: "a",
-            self.dateMonth: "m",
-            self.dateDay: "d",
-            self.energyYear: "kWh",
-            self.energyMonth: "kWh",
-            self.energyDay: "kWh",
-            self.energyTotal: "kWh",
-            self.installedCapacity: "W",
-            self.mainsCycleDuration: "μs",
-            self.networkAddress: "",
-            self.relativeOutput: "%",
-            self.softwareVersion: "",
-            self.voltageDC: "V",
-            self.voltagePhaseOne: "V",
-            self.currentDC: "A",
-            self.temperaturePowerUnitOne: "°C",
+            self.ac_output: "W",
+            self.operating_hours: "h",
+            self.date_year: "a",
+            self.date_month: "m",
+            self.date_day: "d",
+            self.energy_year: "kWh",
+            self.energy_month: "kWh",
+            self.energy_day: "kWh",
+            self.energy_total: "kWh",
+            self.installed_capacity: "W",
+            self.mains_cycle_duration: "μs",
+            self.network_address: "",
+            self.relative_output: "%",
+            self.software_version: "",
+            self.voltage_dc: "V",
+            self.voltage_phase_one: "V",
+            self.current_dc: "A",
+            self.temperature_power_unit_one: "°C",
             self.model: "",
-            self.timeMinutes: "min",
-            self.timeHours: "h",
+            self.time_minutes: "min",
+            self.time_hours: "h",
         }
-        return units[funktion]
+        return units[function]
 
     def model(self) -> str:
         inverter_types = {
@@ -170,7 +169,7 @@ class SolarMax:
             return None
         return status_codes[data]
 
-    def alarmCode(self) -> str:
+    def alarm_code(self) -> str:
         alarm_codes = {
             0: "kein Fehler",
             1: "Externer Fehler 1",
@@ -196,89 +195,89 @@ class SolarMax:
             return None
         return alarm_codes[data]
 
-    def acOutput(self) -> float:
+    def ac_output(self) -> float:
         data = self.query("PAC")
         if data is None:
             return None
         return round(data * 0.5, 1)
 
-    def operatingHours(self) -> int:
+    def operating_hours(self) -> int:
         return self.query("KHR")
 
-    def dateYear(self) -> int:
+    def date_year(self) -> int:
         return self.query("DYR")
 
-    def dateMonth(self) -> int:
+    def date_month(self) -> int:
         return self.query("DMT")
 
-    def dateDay(self) -> int:
+    def date_day(self) -> int:
         return self.query("DDY")
 
-    def energyYear(self) -> int:
+    def energy_year(self) -> int:
         return self.query("KYR")
 
-    def energyMonth(self) -> int:
+    def energy_month(self) -> int:
         return self.query("KMT")
 
-    def energyDay(self) -> float:
+    def energy_day(self) -> float:
         data = self.query("KDY")
         if data is None:
             return None
         return round(data * 0.1, 1)
 
-    def energyTotal(self) -> int:
+    def energy_total(self) -> int:
         return self.query("KT0")
 
-    def installedCapacity(self) -> float:
+    def installed_capacity(self) -> float:
         data = self.query("PIN")
         if data is None:
             return None
         return round(data * 0.5, 1)
 
-    def mainsCycleDuration(self) -> int:
+    def mains_cycle_duration(self) -> int:
         return self.query("TNP")
 
-    def networkAddress(self) -> int:
+    def network_address(self) -> int:
         return self.query("ADR")
 
-    def relativeOutput(self) -> int:
+    def relative_output(self) -> int:
         return self.query("PRL")
 
-    def softwareVersion(self) -> int:
+    def software_version(self) -> int:
         return self.query("SWV")
 
-    def voltageDC(self) -> float:
+    def voltage_dc(self) -> float:
         data = self.query("UDC")
         if data is None:
             return None
         return round(data * 0.1, 1)
 
-    def voltagePhaseOne(self) -> float:
+    def voltage_phase_one(self) -> float:
         data = self.query("UL1")
         if data is None:
             return None
         return round(data * 0.1, 1)
 
-    def currentDC(self) -> float:
+    def current_dc(self) -> float:
         data = self.query("IDC")
         if data is None:
             return None
         return round(data * 0.01, 2)
 
-    def currentPhaseOne(self) -> float:
+    def current_phase_one(self) -> float:
         data = self.query("IL1")
         if data is None:
             return None
         return round(data * 0.01, 2)
 
-    def temperaturePowerUnitOne(self) -> int:
+    def temperature_power_unit_one(self) -> int:
         return self.query("TKK")
 
     def type(self) -> int:
         return self.query("TYP")
 
-    def timeMinutes(self) -> int:
+    def time_minutes(self) -> int:
         return self.query("TMI")
 
-    def timeHours(self) -> int:
+    def time_hours(self) -> int:
         return self.query("THR")

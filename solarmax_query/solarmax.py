f0aabda190adfb410085aced7263c385d1f067ac
Dominik Wetzel <dominik@wetzel.one>
2023-07-02 19:39:54 +0200

    Replace hexValue with format strings

--- solarmax_query/solarmax.py
+++ solarmax_query/solarmax.py
@@ -47,14 +47,12 @@ class SolarMax:
             self.socket = None
             raise Exception(f"Could not connect to host: {self.host}:{self.port}")
 
-    def hexValue(self, i: int) -> str:
-        return (hex(i)[2:]).upper()
 
     def checksum(self, text: str) -> str:
         total = 0
         for c in text:
             total += ord(c)
-        crc = self.hexValue(total)
+        crc = f"{total:04X}"
         while len(crc) < 4:
             crc = "0" + crc
         return crc
@@ -66,16 +64,14 @@ class SolarMax:
         srcAddress = "FB"
 
         # inverterIndex is the index of the inverter we want to query wich is converted to hex wich has to be 2 characters long see 1.1
-        destAddress = self.hexValue(self.index)
-        if len(destAddress) < 2:
-            destAddress = "0" + destAddress
+        destAddress = f"{self.index:02X}"
 
         # this the "port" in hex wich for data query is always 100 see 1.4
         queryType = "64"
 
-        lenght = 1 + 3 + 3 + 3 + 3 + len(code) + 1 + 4 + 1
-        lenght = self.hexValue(lenght)
-        preCrcString = f"{srcAddress};{destAddress};{lenght}|{queryType}:{code}|"
+        length = 1 + 3 + 3 + 3 + 3 + len(code) + 1 + 4 + 1
+        length = f"{length:02X}"
+        preCrcString = f"{srcAddress};{destAddress};{length}|{queryType}:{code}|"
 
         # this is the checksum of the query see 1.1
         crc = self.checksum(preCrcString)
